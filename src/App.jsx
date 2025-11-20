f1977469a8db83cb8599e8fa15d9ee34affb5eb7
avintro <119504876+avintro@users.noreply.github.com>
2025-11-20 22:36:49 +0530

    feat: implement initial application structure with Hero component and video background.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navabar from './components/Navabar'
+import Hero from './components/Hero'
 
 export default function App() {
   return (
     <main>
       <Navabar/>
+      <Hero/>
     </main>
   )
 }
