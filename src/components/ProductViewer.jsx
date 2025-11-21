a94c5fae0e98398e24eb0c9f6f59ac5317d7418c
avintro <119504876+avintro@users.noreply.github.com>
2025-11-22 00:12:24 +0530

    feat: add 3D Macbook models, product viewer, and studio lighting components

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,6 +3,9 @@ import useMackbookStore from '../store'
 import clsx from 'clsx';
 import { Canvas } from '@react-three/fiber';
 import { Box, OrbitControls } from '@react-three/drei';
+import MacbookModel14 from './models/Macbook-14';
+import StudioLights from './StudioLights';
+import MacbookModel16 from './models/Macbook-16';
 
 export default function ProductViewer() {
 
@@ -36,8 +39,8 @@ export default function ProductViewer() {
 
       </div>
         <Canvas id='canvas' camera={{position:[0,2,5], fov:50, near:0.1, far:100}}>
-          <Box position={[-1,1,0]} scale={10*scale} material-color={color}/>
-
+          <StudioLights/>
+          <MacbookModel14 scale={0.06} position={[0,0,0]}/>
           <OrbitControls enableZoom={false}/>
         </Canvas>
     </section>
