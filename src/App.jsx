593bc1ca9221c1e42884b065ae4c603954c49007
avintro <119504876+avintro@users.noreply.github.com>
2025-11-23 00:50:01 +0530

    feat: Implement 3D product viewer with model switching, color customization, and responsive design.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import Navabar from './components/Navabar'
-import Hero from './components/Hero'
-import ProductViewer from './components/ProductViewer'
+import React from "react";
+import Navabar from "./components/Navabar";
+import Hero from "./components/Hero";
+import ProductViewer from "./components/ProductViewer";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function App() {
   return (
     <main>
-      <Navabar/>
-      <Hero/>
-      <ProductViewer/>
+      <Navabar />
+      <Hero />
+      <ProductViewer />
     </main>
-  )
+  );
 }
-   
